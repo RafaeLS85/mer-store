dc4c1abaa39ad08963db9d0cd443d27ff3231ec6
Rafael Olivay <rafaelsebastian@live.com.ar>
2023-01-14 15:59:33 -0300

    add product service

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,11 +1,37 @@
+import { GetStaticProps } from "next";
 import Layout from "../components/layout/Layout";
+import api from "../products/service";
+import { Product } from "../products/types";
 
-export default function Products() {
+interface Props {
+  products: Product[];
+}
+
+export default function Products({ products }: Props) {
+  if (!products) return <div>Loading...</div>;
+
+  console.log({ products });
+
   return (
     <Layout title="Products">
-      <main className="">
+      <main className="text-black ">
         <h1 className="text-3xl font-bold text-black  underline">Products</h1>
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>{product.description}</li>
+          ))}
+        </ul>
       </main>
     </Layout>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const products = await api.list();
+
+  return {
+    props: {
+      products,
+    },
+  };
+};
