4f2208709531e85c69698409a5e5066729a36cbf
Rafael Olivay <rafaelsebastian@live.com.ar>
2023-07-16 20:57:03 -0300

    wtf vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 
-import Layout from "../components/layout/Layout";
+
 import { useSearch } from "../hooks/useSearch";
 import 'react-toastify/dist/ReactToastify.css';
 import Card from "@/components/card/Card";
 import { CategoryStoreInitializer } from "@/components/store/CategoryStoreInitializer";
+import Layout from "@/components/layout/Layout";
 // import { ToastContainer } from 'react-toastify';
 export default function Home() {
   const { data, error } = useSearch();
