0653aff11ed6e0098bf1b621138daed564235a20
Rafael Olivay <rafaelsebastian@live.com.ar>
2023-11-07 18:42:46 -0300

    add styles on change quantity

--- src/components/Checkout/index.tsx
+++ src/components/Checkout/index.tsx
@@ -1,8 +1,5 @@
 "use client";
 import { Chart, Product } from "../../types/types";
-import styles from "./chart.module.css";
-import { IoMdAddCircle } from "react-icons/io";
-import { BiSolidMinusCircle } from "react-icons/bi";
 import { useChartStore } from "../../store/store";
 // import { ToastContainer, toast } from "react-toastify";
 import {
@@ -22,7 +19,6 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 export const CheckoutTable = ({ chart }: { chart: Chart[] }) => {
-  // const { products: data } = useChartStore();
   const chartState = useChartStore();
 
   const notify = () => {
@@ -44,35 +40,42 @@ export const CheckoutTable = ({ chart }: { chart: Chart[] }) => {
   const handleDecrease = (item: Chart) => {    
     if(item.quantity >= 1){
       chartState.decreaseQuantity(item.id)
-    }else{
-      handleRemove(item.id)
     }
   }
 
   return (
     <TableContainer>
       <Table variant="simple">
-        <TableCaption>Imperial to metric conversion factors</TableCaption>
+        <TableCaption>Los precios son solo disponibles para Argentina</TableCaption>
         <Thead>
           <Tr>
+            <Th></Th>
             <Th>Cantidad</Th>
             <Th>Item</Th>
             <Th isNumeric>Precio</Th>
             <Th isNumeric>Disponibles</Th>
-            <Th>Agregar/Borrar</Th>
+            <Th></Th>
           </Tr>
         </Thead>
         <Tbody>
           {
             chartState.chart.map((item, i) => (
               <Tr key={ item.id + i }>
+                 <Td style={{ display: 'flex', gap: '5px' }}>
+                  <button onClick={()=> chartState.increaseQuantity(item.id)} style={{
+                     opacity: item.quantity < item.stock ? '1' : '0.5',
+                     pointerEvents: item.quantity !== item.stock ? 'unset' : 'none'
+                  }} ><TriangleUpIcon color="green.500" /></button>
+                  <button onClick={() => handleDecrease(item)} style={{
+                    opacity: item.quantity !== 1 ? '1' : '0.5',
+                    pointerEvents: item.quantity !== 1 ? 'unset' : 'none'
+                  }}><TriangleDownIcon color="red.500" /></button>
+                </Td>
                 <Td>{item.quantity}</Td>
                 <Td>{item.title}</Td>
-                <Td>{item.price}</Td>
+                <Td>${item.price}</Td>
                 <Td>{item.stock}</Td>
                 <Td>
-                  <button onClick={()=> chartState.increaseQuantity(item.id)}><TriangleUpIcon color="green.500" /></button>
-                  <button onClick={() => handleDecrease(item)}><TriangleDownIcon color="red.500" /></button>
                   <button onClick={() =>handleRemove(item.id)}><DeleteIcon /></button>
                 </Td>
               </Tr>
